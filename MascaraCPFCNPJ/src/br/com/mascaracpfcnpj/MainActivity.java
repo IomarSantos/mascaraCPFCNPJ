da1507787b6afa8f2d0cca44aa1da5f97a6b2521
Iomar Santos <iomar.santos@outlook.com>
2014-11-04 04:19:32 -0200

    Commit inicial

---
+++ MascaraCPFCNPJ/src/br/com/mascaracpfcnpj/MainActivity.java
@@ -0,0 +1,47 @@
+package br.com.mascaracpfcnpj;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+
+public class MainActivity extends Activity {
+	private TextWatcher cpfMask;
+	private TextWatcher cnpjMask;
+	private RadioGroup radioGroup;
+	private RadioButton rdCNPJ;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		final EditText cpf = (EditText) findViewById(R.id.txtCPF);
+		
+		// Armazene seus TextWatcher para posterior uso
+		cpfMask = Mask.insert("###.###.###-##", cpf);
+		cpf.addTextChangedListener(cpfMask);
+
+		cnpjMask = Mask.insert("##.###.###/####-##", cpf);
+		rdCNPJ = (RadioButton) findViewById(R.id.rdCNPJ);
+
+		radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+		radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				int opcao = radioGroup.getCheckedRadioButtonId();
+				if (opcao == rdCNPJ.getId()) {
+					cpf.removeTextChangedListener(cpfMask);
+					cpf.addTextChangedListener(cnpjMask);
+				} else {
+					cpf.removeTextChangedListener(cnpjMask);
+					cpf.addTextChangedListener(cpfMask);
+				}
+			}
+		});
+
+	}
+}
